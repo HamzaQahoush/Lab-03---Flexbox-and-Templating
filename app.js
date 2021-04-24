bad2d7f28a34d83dc9366c0801d6dff96135bab6
HamzaQahoush <hamza.qah@gmail.com>
2021-04-24 23:27:33 +0300

    fix sort buttons

--- app.js
+++ app.js
@@ -24,18 +24,17 @@ ShowPhoto.prototype.renderObj=function(){
 
 function getInfo(arr) {
 
-
   for (let i = 0; i < arr.length; i++) {
     if (arrayData.indexOf(arr[i]) === -1) {
       arrayData.push(arr[i]);
+      console.log(arrayData);
     }
   }
-
 }
 
 
 function selection(){
-  $('select').append('<option value= "all" id= "option"> Keywords Search</option>');
+  $('select').append('<option value= "" id= "option"> Keywords Search</option>');
 
   for (let i=0 ; i<arrayData.length;i++){
     let option = $('#option').clone();
@@ -45,13 +44,42 @@ function selection(){
     option.attr('value' , arrayData[i]);
 
   }
-
+  //on change selection hide every div AND show selected value
   $('#select').on('change', function () {
     $('div').css({ 'display': 'none' });
 
-    $(`.${this.value}`).css({ 'display': 'inline-block' });
+    $( `.${this.value}`).css({ 'display': 'inline-block' });
   });
 }
+
+
+$('#sort1').on('click', function () {
+  $('div').remove();
+  imgArr.sort((a, b) => {
+    if (a.title.toLowerCase() > b.title.toLowerCase()) return 1;
+    else if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;
+    else return 0;
+  });
+  //render sorting data in template
+  imgArr.forEach((value) => {
+    $('span').append(value.renderObj());
+  });
+});
+
+$('#sort2').on('click', function () {
+  imgArr.sort((a, b) => {
+    $('div').remove();
+    return a.horns - b.horns;
+  });
+  imgArr.forEach(value => {
+    $('span').append(value.renderObj());
+  });
+});
+
+
+
+
+
 
 
 ShowPhoto.getJson1 = () => {
@@ -66,7 +94,7 @@ ShowPhoto.getJson1 = () => {
 
     data.forEach((element) => {
       let horn = new ShowPhoto (element);
-      $('#Items').append(horn.renderObj());
+      $('#allItems').append(horn.renderObj());
     });
     getInfo(findArr);
     selection();
@@ -85,7 +113,7 @@ ShowPhoto.getJson2 = () => {
 
     data.forEach((element) => {
       let horn = new ShowPhoto(element);
-      $('#Items').append(horn.renderObj());
+      $('#allItems').append(horn.renderObj());
     });
     getInfo(findArr);
     selection();
@@ -95,7 +123,7 @@ ShowPhoto.getJson2 = () => {
 
 
 
-$(() => ShowPhoto.getJson1());
+ShowPhoto.getJson1();
 
 function page1() {
   $('.all').remove();
